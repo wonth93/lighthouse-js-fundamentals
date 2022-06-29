a6b42c308d51c524170d61b19e17b1173dec7c42
wonth93 <wongth93@gmail.com>
2022-06-29 03:48:07 +0000

    packing while loop

--- packing-list.js
+++ packing-list.js
@@ -6,8 +6,3 @@ while (i < packingList.length) {
   console.log(packingList[i]);
   i++;
 }
-
-console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
-}
